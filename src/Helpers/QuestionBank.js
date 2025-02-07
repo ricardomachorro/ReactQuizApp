fe446cd6c99644c181bc8a991dd6af9bd446fcf1
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2025-02-06 21:03:02 -0600

    First big update

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,29 @@
+export const Questions =[
+
+    {
+        prompt :"What is 1+3",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C"
+    },
+    {
+        prompt :"Which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robinhood",
+        answer: "B"
+    },
+    {
+        prompt :"What animal is green",
+        optionA: "Elephant",
+        optionB: "Bear",
+        optionC: "Bee",
+        optionD: "Crocodile",
+        answer: "D"
+    }
+
+
+]
